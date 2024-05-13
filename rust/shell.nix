0ebd03f3ca435292a8e3b15533a96349b3d732fd
Yvonne Aizawa <test@mail.com>
2024-05-13 20:39:44 +0200

    initial commit
    currently just rust. might add others

---
+++ rust/shell.nix
@@ -0,0 +1,15 @@
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+in
+with pkgs;
+mkShell {
+  buildInputs = [
+    gdb
+    picocom
+    udev pkg-config
+    openssl
+    (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+  ];
+  RUST_LOG = "info";
+}
